eb56400f7a4fd1b62bc1cd50f4d8816cd94ea7f9
BaykinDF <bdf@bazis-itc.ru>
2023-11-13 22:51:21 +0300

    step 3-3 DeadLock v2

---
+++ src/main/java/course/concurrency/m3_shared/DeadLock.java
@@ -0,0 +1,39 @@
+package course.concurrency.m3_shared;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+
+public class DeadLock {
+
+    private static final Map<String, Integer> map = new ConcurrentHashMap<>(16);
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(()->{DeadLock.block("res1","res2");});
+        Thread t2 = new Thread(()->{DeadLock.block("res2","res1");});
+        t1.start();
+        t2.start();
+    }
+
+    private static void block(final String a,final String b ) {
+        map.computeIfAbsent(a, k -> {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace( System.err);
+            }
+            return  map.computeIfAbsent(b, l -> 5);
+        });
+    }
+
+
+
+}
+
+
+
+
+
+
+
